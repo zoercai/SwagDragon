0faca6f16d09e148063816239c23cfaad417c5a2
Michael <mste690@gmail.com>
2015-08-08 15:29:17 +1200

    Added creation of treeList from csv resources file

--- app/src/main/java/com/example/zoe/swagdragon/MapsActivity.java
+++ app/src/main/java/com/example/zoe/swagdragon/MapsActivity.java
@@ -1,12 +1,21 @@
 package com.example.zoe.swagdragon;
 
+import android.content.res.AssetManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -17,6 +26,7 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        setUpTreeData();
     }
 
     @Override
@@ -61,5 +71,26 @@ public class MapsActivity extends FragmentActivity {
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    private void setUpTreeData() {
+        try {
+            AssetManager assetManager = getAssets();
+            InputStream csvStream = assetManager.open("app/src/main/assets/PUP_NotableTrees.csv");
+            InputStreamReader csvStreamReader = new        InputStreamReader(csvStream);
+            CSVReader csvReader = new CSVReader(csvStreamReader);
+            List treeList = new ArrayList<String[]>();
+            String[] line;
+
+            // throw away the header
+            csvReader.readNext();
+
+            while ((line = csvReader.readNext()) != null) {
+                treeList.add(line);
+                Log.v("line", line+"");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
